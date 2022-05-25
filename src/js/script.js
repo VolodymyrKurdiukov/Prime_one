7831516a2df573921718af5948d3bb46a63ae254
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-05-25 15:32:12 +0300

    private+products

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,11 @@ closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
 
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
